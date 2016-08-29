a484c67e541eae73af9fdddd079ee7367ccbfdbe
Sergio Mattioli <sergiomattioli@gmail.com>
2016-08-28 21:08:50 -0300

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name eggheadAngularjsFromScratchGettingStarted01BootstrapApp
+ * @description
+ * # eggheadAngularjsFromScratchGettingStarted01BootstrapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('eggheadAngularjsFromScratchGettingStarted01BootstrapApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eggheadAngularjsFromScratchGettingStarted01BootstrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eggheadAngularjsFromScratchGettingStarted01BootstrapApp
+ */
+angular.module('eggheadAngularjsFromScratchGettingStarted01BootstrapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
